520362410ee7de4f9ab496c444a0f2881789fe3b
Ganesan, Vishnu <visganesan@informatica.com>
2021-04-13 17:29:36 +0530

    Added progressbar

--- app.py
+++ app.py
@@ -1,30 +1,42 @@
 '''
-pip install idle-time
-pip install pyautogui
+Install the dependencies using
+	pip install -r requirements.txt
+
+or install it manuall
+	pip install idle-time
+	pip install pyautogui
+	pip install progressbar
 '''
 
 from idle_time import IdleMonitor
 import time
 import pyautogui
+import progressbar
 
-
-#In seconds
-INTERVAL_TO_CHECK = 20
-ALLOWED_IDLE_TIME = 180
-
+ALLOWED_IDLE_TIME = 180 #In seconds
 
 def main():
-	monitor = IdleMonitor.get_monitor()
+	while True:
+		forever()
 
-	while(True):
-		time.sleep(INTERVAL_TO_CHECK)
+def forever():
+	monitor = IdleMonitor.get_monitor()
+	prevTime = monitor.get_idle_time();
+	bar = progressbar.ProgressBar(maxval=ALLOWED_IDLE_TIME, widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+	bar.start()
+	while True:
+		time.sleep(1)
 		idleTime = monitor.get_idle_time()
-		print(idleTime)
+		if prevTime > idleTime:
+			bar.start()
+		bar.update(min(idleTime + 1, ALLOWED_IDLE_TIME))
 		if(idleTime > ALLOWED_IDLE_TIME):
 			pyautogui.press('win')
+			time.sleep(0.15) #necessary else two keypresses sometimes getting registered as one
 			pyautogui.press('win')
-			print("You were afk for {} seconds, but no worries".format(idleTime))
-
+			bar.finish()
+			print("You were afk for {} seconds, sending some keystrokes".format(idleTime))
+			return
 
 if __name__ == "__main__":
 	main()
